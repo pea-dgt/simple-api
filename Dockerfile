59a018e847042c74045f3579a2b44ffd355dcef3
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2024-05-09 21:58:45 +0700

    add: charger list

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,20 @@
-FROM --platform=linux/amd64 node:18
+FROM --platform=linux/amd64 node:18 AS builder
 
-RUN apt update && apt upgrade -y
-RUN npm install -g npm@10.4.0
+# RUN apt update && apt upgrade -y
+# RUN npm install -g npm@10.4.0
+
+FROM --platform=linux/amd64 gcr.io/distroless/nodejs18-debian12:nonroot AS runner
 
 WORKDIR /home/node
 COPY . .
 RUN npm install
 
-RUN useradd nobody
-USER nobody
+# USER nobody
 
-RUN chown -R /home/node
-RUN chmod +x /home/node/run.sh
+# RUN chown -R nobody /home/node
+# RUN chmod +x /home/node/run.sh
 
 EXPOSE 3000
 
-ENTRYPOINT [ "sh", "/home/node/run.sh" ]
+ENTRYPOINT [ "/home/node/index.js" ]
+# ENTRYPOINT [ "bash", "/home/node/run.sh" ]
--- express.js
+++ express.js
@@ -1,25 +1,25 @@
 // Dotenv initial
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Express initial
-const express = require("express")
-const bodyParser = require("body-parser")
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 // Redis initial
-const { createClient } = require('redis')
+const { createClient } = require('redis');
 
 // Mongo global config
-const { createMongoDBConnection } = require('./libs/mongodb')
+const { createMongoDBConnection } = require('./libs/mongodb');
 
 // Express global config
-app.use(bodyParser.json())
-bodyParser.urlencoded({ extended: true })
+app.use(bodyParser.json());
+bodyParser.urlencoded({ extended: true });
 
 if (!app.locals.mongo) {
   console.log(`Initialize MongoDB`);
-  app.locals.mongo = createMongoDBConnection()
+  app.locals.mongo = createMongoDBConnection();
 }
 
 // async function initialRedisConnection () {
@@ -55,23 +55,42 @@ app.get('/health', (_, res) => {
 })
 
 app.get('/mongo', async (req, res) => {
+  const { mongo: client }  = req.app.locals;
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    const { mongo: client }  = req.app.locals
-    console.log('Connecting to Mongo server...');
     await client.connect();
 
     // Send a ping to confirm a successful connection
-    await client.db("volta-poc").command({ ping: 1 });
+    await client.db("volta-db").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    res.status(200).json({})
+    res.status(200).json({});
   } catch (error) {
-    console.error(error)
+    console.error(error);
+    res.status(500).json({});
+  } finally {
     await client.close();
-    res.status(500).json({})
-    // Ensures that the client will close when you finish/error
   }
-})
+});
+
+app.get('/chargers', async (req, res) => {
+  const { mongo: client }  = req.app.locals;
+
+  try {
+    await client.connect();
+
+    const { chargerId } = req.query;
+    const db = client.db("volta-db");
+    const chargersCollection = db.collection("chargers");
+    const chargers =  await chargersCollection.find({ chargerId }).toArray();
+    
+    res.status(200).json({ chargers });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  } finally {
+    await client.close();
+  }
+});
 
 module.exports = app
